e9551b6b1c6810d8b86dc7570b32001edc888655
m-tehrani <t.mohammadamin@gmail.com>
2022-02-09 19:50:49 +0330

    Custom Model manager

--- core/models.py
+++ core/models.py
@@ -16,16 +16,31 @@ def brand_validator(value: str):
         raise ValidationError("The brand name should start with an uppercase letter!")
 
 
+class BaseManager(models.Manager):
+
+    def full_archive(self):
+        return super().get_queryset()
+
+    def get_queryset(self):
+        return super().get_queryset().exclude(is_delete=True)
+
+
 class BaseModel(models.Model):
     class Meta:
         abstract = True
 
-    is_delete = models.BooleanField(default=False)
+    objects = BaseManager()
+
+    is_delete = models.BooleanField(default=False, editable=False, db_index=True)
 
     def delete(self, using=None, keep_parents=False):
         self.is_delete = True
         self.save(using=using)
         # return super().delete(using, keep_parents)
+
+    def restore(self):
+        self.is_delete = False
+        self.save()
 
 
 class Brand(BaseModel):
@@ -33,7 +48,7 @@ class Brand(BaseModel):
                             help_text="The brand's name", verbose_name='Brand name',
                             validators=[brand_validator, validators.MaxLengthValidator(10, "Shorten your brand name!")])
     country = models.CharField(max_length=20)
-    image = models.FileField(null=True, default=None, upload_to='brand/',blank=True)
+    image = models.FileField(null=True, default=None, upload_to='brand/', blank=True)
 
     def __str__(self):
         return f"Brand #{self.id}: {self.name} from {self.country}"
